c2c1618d332bd5708acaa1ce25668a993f28addd
errorr404 <dkbishwash@gmail.com>
2019-03-21 18:13:36 +0530

    Updates

---
+++ precache-manifest.fbe0f2d053a4718626c50359f06b30af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fbcc3273937e91c29a1d",
+    "url": "/portfolio/static/js/main.3185370b.chunk.js"
+  },
+  {
+    "revision": "f7ce096203fdb3ae4a7a",
+    "url": "/portfolio/static/js/2.760b29d1.chunk.js"
+  },
+  {
+    "revision": "fbcc3273937e91c29a1d",
+    "url": "/portfolio/static/css/main.77932b03.chunk.css"
+  },
+  {
+    "revision": "f7ce096203fdb3ae4a7a",
+    "url": "/portfolio/static/css/2.ed1da59f.chunk.css"
+  },
+  {
+    "revision": "0ca68e882e26f4b71e94fa661dd5d2ad",
+    "url": "/portfolio/index.html"
+  }
+];
