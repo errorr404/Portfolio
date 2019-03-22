56f70bc95ed31925aac733931fd04def164348cf
errorr404 <dkbishwash@gmail.com>
2019-03-22 13:21:05 +0530

    Updates

--- precache-manifest.e34d7a07da28093d72c8fe792d7e0e00.js
+++ precache-manifest.e34d7a07da28093d72c8fe792d7e0e00.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
   },
   {
-    "revision": "a2d3a8c358978c2b26b9",
-    "url": "/portfolio/static/js/main.cfb89358.chunk.js"
+    "revision": "1cf55b9b7399965448e0",
+    "url": "/portfolio/static/js/main.affc68f4.chunk.js"
   },
   {
     "revision": "f7ce096203fdb3ae4a7a",
     "url": "/portfolio/static/js/2.760b29d1.chunk.js"
   },
   {
-    "revision": "a2d3a8c358978c2b26b9",
-    "url": "/portfolio/static/css/main.e7e8d70c.chunk.css"
+    "revision": "1cf55b9b7399965448e0",
+    "url": "/portfolio/static/css/main.455bdaaf.chunk.css"
   },
   {
     "revision": "f7ce096203fdb3ae4a7a",
     "url": "/portfolio/static/css/2.ed1da59f.chunk.css"
   },
   {
-    "revision": "6459015f3960b8fe028a032fd26a78fb",
+    "revision": "fc97ae8a423146cdeea8c6b3ab9034c9",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.eb853851c19d2cf48b0ae6e99014d5c3.js"
+  "/portfolio/precache-manifest.e34d7a07da28093d72c8fe792d7e0e00.js"
 );
 
 workbox.clientsClaim();
